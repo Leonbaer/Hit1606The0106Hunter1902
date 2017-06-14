d1c040b4918ac38d0f337582ddfac1539bd00129
leon bär <leonbar@leons-macbook-pro.local>
2017-06-14 16:45:08 +0200

    Initial Commit

---
+++ hit the hunterTests/hit_the_hunterTests.swift
@@ -0,0 +1,36 @@
+//
+//  hit_the_hunterTests.swift
+//  hit the hunterTests
+//
+//  Created by leon bär on 03.06.17.
+//  Copyright © 2017 Leon_baer. All rights reserved.
+//
+
+import XCTest
+@testable import hit_the_hunter
+
+class hit_the_hunterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
