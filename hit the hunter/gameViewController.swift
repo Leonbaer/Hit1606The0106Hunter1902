8462ce4115b922ba20f9044196b6c594e8109acc
Janne Krellig <Janne@jannes-air>
2017-06-26 16:33:04 +0200

    test: Button stürzt weiterhin ab

--- hit the hunter/gameViewController.swift
+++ hit the hunter/gameViewController.swift
@@ -1,67 +1,48 @@
 //
-//  ViewController.swift
+//  gameViewController.swift
 //  hit the hunter
 //
-//  Created by leon bär on 03.06.17.
+//  Created by Janne Krellig on 26.06.17.
 //  Copyright © 2017 Leon_baer. All rights reserved.
 //
 
 import UIKit
 import AVFoundation
 
-
-class ViewController: UIViewController {
-
+class gameViewController: UIViewController {
     
-    
-    
-   
     var audioPlayer = AVAudioPlayer()
     var audioGame = AVAudioPlayer()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-              
+        
+        
         do{
             
             audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Seren", ofType: "mp3")!))
             audioPlayer.prepareToPlay()
-            audioPlayer.play()
             
-                
-               
+            
+            
         }
-    catch{
+        catch{
             
             print("app neu starten")
         }
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-       @IBAction func play(_ sender: Any) {
-    audioPlayer.stop()
-        do{
-            
-            audioGame = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Move", ofType: "mp3")!))
-            audioGame.prepareToPlay()
-            audioGame.play()
-        }
-        catch{
-            
-            print("app neu starten")
-        }
-       _ = true
-     
-               
-        
-        }
     
-    
-    
+
+    @IBAction func stop(_ sender: Any) {
+        audioGame.stop()
     }
-
-
-
+   
+}
