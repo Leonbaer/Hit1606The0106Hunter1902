a408b76becaa1b09e0ff9f4b645219d805468f2f
Janne Krellig <Janne@jannes-air>
2017-06-27 16:23:47 +0200

    Kokosnuss im Bild, code für animation enthalten, app stürzt trotzdem ab

--- hit the hunter/gameViewController.swift
+++ hit the hunter/gameViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 import AVFoundation
+import Darwin
 
 class gameViewController: UIViewController {
     
         var audioGame = AVAudioPlayer()
+        var audioSchuss = AVAudioPlayer()
+    
+    
+    @IBOutlet weak var kokosnuss: UIImageView!
+    
 
     
     override func viewDidLoad() {
@@ -23,14 +29,26 @@ class gameViewController: UIViewController {
             audioGame = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Move", ofType: "mp3")!))
             audioGame.prepareToPlay()
             audioGame.play()
-        }
+            
+                   }
         catch{
             
             print("app neu starten")
         }
+    
+        do {
+            
+            audioSchuss = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "affe", ofType: "mp3")!))
+            audioSchuss.prepareToPlay()
+        }
+        catch{
+            print("app neu starten")
+        }
+        }
+    
         
-    }
-
+        
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,6 +57,19 @@ class gameViewController: UIViewController {
 
     @IBAction func stop(_ sender: Any) {
         audioGame.stop()
+        
+     
+        
     }
-   
+    
+    @IBAction func schuss(_ sender: Any) {
+     
+        UIImageView.animate(withDuration: 0.5, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: {
+            self.kokosnuss.frame.origin.y = 160
+        }, completion: { (finished: Bool) in
+            print("Animation Ended!")
+        });
+    }
+    
+       
 }
